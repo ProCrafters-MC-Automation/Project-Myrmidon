985a08e961aab080b1bfdc98988552285d4bd161
phsadhankar <73192264+phsadhankar@users.noreply.github.com>
2026-06-22 01:10:12 -0500

    Initial commit

---
+++ src/client/java/org/pmca/project_myrmidon/client/Project_myrmidonClient.java
@@ -0,0 +1,10 @@
+package org.pmca.project_myrmidon.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Project_myrmidonClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/pmca/project_myrmidon/Project_myrmidon.java
@@ -0,0 +1,10 @@
+package org.pmca.project_myrmidon;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Project_myrmidon implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
